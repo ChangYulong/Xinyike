6d534794d7fa62115e73c52514c44de2a562ba25
jcwymac <jcwymac@jcwymacdeMac-mini.local>
2017-11-16 14:02:31 +0800

    你好，哈哈

--- JCXinYiKe/AppDelegate.swift
+++ JCXinYiKe/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        //你好
         window = UIWindow.init()
         let tabBarController = TabBarController.init()
         window?.rootViewController = tabBarController
